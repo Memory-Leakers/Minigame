a965e75fcd99e0f819b40256d0c95f2b0afbeecc
Yogui <972081300@qq.com>
2021-03-09 23:37:51 +0100

    First Update

---
+++ Source/MyLib.h
@@ -0,0 +1,8 @@
+#include <stdio.h>			// Required for: printf()
+#include <stdlib.h>			// Required for: EXIT_SUCCESS
+#include <math.h>			// Required for: sinf(), cosf()
+
+// Include SDL libraries
+#include "SDL/include/SDL.h"				// Required for SDL base systems functionality
+#include "SDL_image/include/SDL_image.h"	// Required for image loading functionality
+#include "SDL_mixer/include/SDL_mixer.h"
