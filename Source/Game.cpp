cf4622ba132da5d48da6435de1ab8417630c0945
Loproxi <79161178+Loproxi@users.noreply.github.com>
2021-03-12 18:03:51 +0100

    Key System

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -8,12 +8,34 @@
 
 #define MAX_KEYBOARD_KEYS 256
 #define MAX_MOUSE_BUTTONS 5
+enum WindowEvent
+{
+	WE_QUIT = 0,
+	WE_HIDE,
+	WE_SHOW,
+	WE_COUNT
+};
+bool window_events[WE_COUNT];
+enum KeyState
+{
+	KEY_IDLE = 0,		// DEFAULT
+	KEY_DOWN,			// PRESSED (DEFAULT->DOWN)
+	KEY_REPEAT,			// KEEP DOWN (sustained)
+	KEY_UP				// RELEASED (DOWN->DEFAULT)
+};
+KeyState keys[MAX_KEYBOARD_KEYS];
+KeyState mouse_buttons[MAX_MOUSE_BUTTONS];
+int mouse_x;
+int mouse_y;
 
 bool Game::Init(Display Disp) {
 
 	canvas = Disp;
 	
 	return canvas.createDisplay(SCREEN_WIDTH, SCREEN_HEIGHT);;
+	//Initialize keys array
+	for (int i = 0; i < MAX_KEYBOARD_KEYS; ++i)
+		keys[i] = KEY_IDLE;
 }
 
 void Game::Draw() {
@@ -37,14 +59,74 @@ void Game::Draw() {
 	SDL_Delay(10);	// 1000/10 = 100 fps max
 }
 
-bool Game::Input() {
+bool Game::Input()
+{
 	SDL_Event event;
+	
+	for (int i = 0; i < MAX_MOUSE_BUTTONS; ++i)
+	{
+		if (mouse_buttons[i] == KEY_DOWN) mouse_buttons[i] = KEY_REPEAT;
+		if (mouse_buttons[i] == KEY_UP) mouse_buttons[i] = KEY_IDLE;
+	}
 
-	if (SDL_PollEvent(&event)) {
-		if (event.type == SDL_QUIT) {
-			return false;
+	while (SDL_PollEvent(&event) != 0)
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT: window_events[WE_QUIT] = true; break;
+		case SDL_WINDOWEVENT:
+		{
+			switch (event.window.event) {
+
+				//case SDL_WINDOWEVENT_LEAVE:
+			case SDL_WINDOWEVENT_HIDDEN:
+			case SDL_WINDOWEVENT_MINIMIZED:
+			case SDL_WINDOWEVENT_FOCUS_LOST: window_events[WE_HIDE] = true; break;
+				//case SDL_WINDOWEVENT_ENTER:
+			case SDL_WINDOWEVENT_SHOWN:
+			case SDL_WINDOWEVENT_FOCUS_GAINED:
+			case SDL_WINDOWEVENT_MAXIMIZED:
+			case SDL_WINDOWEVENT_RESTORED: window_events[WE_SHOW] = true; break;
+			case SDL_WINDOWEVENT_CLOSE: window_events[WE_QUIT] = true; break;
+			default: break;
+			}
+		} break;
+		//Comprobar estado del boton
+		case SDL_MOUSEBUTTONDOWN: mouse_buttons[event.button.button - 1] = KEY_DOWN; break;
+		case SDL_MOUSEBUTTONUP:	mouse_buttons[event.button.button - 1] = KEY_UP; break;
+		case SDL_MOUSEMOTION:
+		{
+			mouse_x = event.motion.x;
+			mouse_y = event.motion.y;
+		} break;
+		default: break;
+		}
+
+	}
+	const Uint8* Keys = SDL_GetKeyboardState(NULL);
+
+	
+	// Consider previous keys states for KEY_DOWN and KEY_UP
+	for (int i = 0; i < MAX_KEYBOARD_KEYS; ++i)
+	{
+		// Valor 1 = tecla esta pulsada , valor 0 no lo esta.
+		if (Keys[i] == 1)
+		{
+			if (keys[i] == KEY_IDLE) keys[i] = KEY_DOWN;
+			else keys[i] = KEY_REPEAT;
+		}
+		else
+		{
+			if (keys[i] == KEY_REPEAT || keys[i] == KEY_DOWN) keys[i] = KEY_UP;
+			else keys[i] = KEY_IDLE;
 		}
 	}
+
+	
+	if (keys[SDL_SCANCODE_ESCAPE] == KEY_DOWN) return false;
+
+	// Check QUIT window event to finish the game
+	if (window_events[WE_QUIT] == true) return false;
 
 	return true;
 }
--- Source/Game.h
+++ Source/Game.h
@@ -1,14 +1,18 @@
 #pragma once
 
 #include "Display.h"
+#define MAX_KEYBOARD_KEYS 256
 
 class Game {
 	private:
 		Display canvas;
+		SDL_Window* window;
+		
 
 	public:
 		bool Init(Display Disp);
 		void Draw();
 		bool Tick();
 		bool Input();
+		
 };
