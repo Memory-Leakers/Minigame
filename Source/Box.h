30e70adcacea3bf9607faf9a924d70382d5021ba
Yogui <972081300@qq.com>
2021-03-15 02:36:01 +0100

    Update

--- Source/Box.h
+++ Source/Box.h
@@ -1,6 +1,15 @@
 #pragma once
 #include "Entity.h"
 
+struct TreeAnim {
+
+	SDL_Texture* idle[2];
+
+	double startTime;
+	float changeTime;
+
+	//SDL_Texture* death;
+};
 
 class Box : public Entity {
 	using Entity::Entity;
